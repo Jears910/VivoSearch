def9fd52b8cc6cd4ad7884e1a50830724f75a91a
S22f5 <51321684+S22f5@users.noreply.github.com>
2020-02-08 18:02:38 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+//dependencies
+var Combinatorics = require('js-combinatorics');
+var Math = require('math');
+var Cheerio = require('cheerio');
+var Crawler = require('crawler');
+//code | videoIDgen
+function changeVideoID(ID) {
+  var VideoID = ID
+}
+console.log(Combinatorics.baseN(['a','b','c'], 10).toArray())
+//code | URL
+const Base = 'https://vivo.sx/'
+var VideoID =  '2d3930ac27'
+//code | CrawlVivo
+var c = new Crawler({
+    maxConnections : 10,
+    callback : function (error, res, done) {
+        if(error){
+            console.log(error);
+        }else{
+            var $ = res.$;
+            // $ is Cheerio by default
+            var RawTitle = $("h1").text()
+            var Proc1Title = RawTitle.split('(')[0]
+            var Title = Proc1Title.slice(6, -1)
+            console.log('Currently at: ' + VideoID +' Called: '+ Title)
+            }
+        done();
+    }
+});
+//code | VideoName
+c.queue(Base+VideoID);
+//code | debugging
